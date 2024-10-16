86dc36f1a2c576948647a673768456c75219d417
Your Name <you@example.com>
2024-10-16 11:48:44 +0530

    Commit

---
+++ src/ex_01_15102024/test_Lab01.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import allure
+
+
+@allure.title("Verify the Title of the page")
+def test_sample():
+     driver = webdriver.Chrome()
+     driver.get("https://katalon-demo-cura.herokuapp.com/")
+     print(driver.title)
+     assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/"
+     assert driver.title == "CURA Healthcare Service"
